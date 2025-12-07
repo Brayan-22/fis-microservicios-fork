823e0aea5dbaf4c37a3569ea7d40f1b1ac3c2fdc
HanaPinilla <hana.pinilla@unp.gov.co>
2025-12-07 18:38:04 -0500

    Adicion de CORS configuracion

--- multimedia/src/main/java/uni/fis/multimedia/config/WebConfig.java
+++ multimedia/src/main/java/uni/fis/multimedia/config/WebConfig.java
@@ -1,6 +1,8 @@
 package uni.fis.multimedia.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,5 +14,21 @@ public class WebConfig implements WebMvcConfigurer {
         registry
             .addResourceHandler("/media/**")
             .addResourceLocations("file:/uploads/");
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // aplica a toda la app multimedia
+                        .allowedOrigins(
+                                "*"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
